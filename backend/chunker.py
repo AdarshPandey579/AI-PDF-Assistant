47f9b500db51bd7e5d01a1dbf380ddf263661718
AdarshPandey579 <ap5989318@gmail.com>
2026-07-19 23:52:01 +0530

    Adjust Chunk size

--- backend/chunker.py
+++ backend/chunker.py
@@ -2,8 +2,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def split_documents(documents):
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=3000,
-        chunk_overlap=300
+        chunk_size=1000,
+        chunk_overlap=200
     )
 
     chunks = splitter.split_documents(documents)
